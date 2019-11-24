b77ae75d917780848469e479466f557f6f94a6d3
My Laptop <My Laptop@MyLaptop-PC>
2019-11-24 18:28:48 +0530

    Day 2 Classroom

--- src/main/java/day2/classroom/arrays/MissingElement.java
+++ src/main/java/day2/classroom/arrays/MissingElement.java
@@ -4,18 +4,18 @@ import java.util.Arrays;
 
 public class MissingElement {
 
-	
+
 	public static void main(String[] args) {
 
 		// Here is the input
-		int[] data = {3,2,4,6,7,8};
+		int[] data = {3,2,4,6,5,8};
 		Arrays.sort(data);
-		
-		for (int i = data[0] ; i<= data.length-1 ;  i++) {
-			
-			if (i+1!=data[i+1]) {
-				 System.out.println("Missing Element is  " + (data[i] + 1)) ;
-				 break;
+
+		for (int i = 0 ; i<= data.length-1 ;  i++) {
+
+			if (data [i] +1!=data[i+1]) {
+				System.out.println("Missing Element is  " + (data[i] +1 )) ;
+				break;
 			}
 		}
 		/*
@@ -26,16 +26,16 @@ public class MissingElement {
 		 		the next one should be = previous one + 1
 		 4) If the following one is not +1 -> Print as missing
 		 */
-		
-		
-		
-		
-		
-		
-		
+
+
+
+
+
+
+
 		// Print the second largest number	
-		
-		
+
+
 	}
 
 }
